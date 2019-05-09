367e909a2112725a98bad1eb1c379207eb185d96
David Goldstein <dgoldstein01@gmail.com>
2019-05-08 21:38:25 -0400

    commit 2

--- problem_1.c
+++ problem_1.c
@@ -11,6 +11,11 @@ double consecutiveSum(int step, double max) {
 	return step * floor((pow(max,2) + max) / 2);
 } 
 
+/**
+ * Main function
+ * finds consecutive sum of integers 0 -> 999 with a step of 5 + 
+ *	integers 0 -> 999 with a step of 3
+ **/
 int main() {
 	printf("%f \n",consecutiveSum(3, 999) + consecutiveSum(5, 999) - consecutiveSum(15, 999));
 	return 0;
