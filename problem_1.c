27b101117067c8a0e611bd0b9fd6a7c5227e8139
David Goldstein <dgoldstein01@gmail.com>
2019-05-08 21:43:38 -0400

    commit 6

--- problem_1.c
+++ problem_1.c
@@ -2,16 +2,6 @@
 #include <math.h>
 
 /**
- * @param {int} (i.e. compute sum of n + (n + step))
- * @param {double} max
- * @return {double} sum of all consecutive integers within range
- */
-double consecutiveSum(int step, double max) {
-	max = floor(max / step);
-	return step * floor((pow(max,2) + max) / 2);
-} 
-
-/**
  * Main function
  * finds consecutive sum of integers 0 -> 999 with a step of 5 + 
  *	integers 0 -> 999 with a step of 3
