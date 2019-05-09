f6d88ce00297110b722aafc4204161fe5215f71a
David Goldstein <dgoldstein01@gmail.com>
2019-05-08 21:36:38 -0400

    rewrite comments

--- problem_1.c
+++ problem_1.c
@@ -2,6 +2,11 @@
 #include <math.h>
 
 /**
+ * 
+ * gets sum of integers from 0 -> max with a step
+ * based on fibonacci recurrance relation (n^2 + n) / 2, 
+ * where n is (n / step) sum is product times step
+ *  
  * @param {int} (i.e. compute sum of n + (n + step))
  * @param {int} max
  * @return {int} sum of all consecutive integers within range
