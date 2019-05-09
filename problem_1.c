ce1ae506057860f75d1a0037fbd2bf29d2af225d
David Goldstein <dgoldstein01@gmail.com>
2019-05-08 21:41:44 -0400

    commit 4

--- problem_1.c
+++ problem_1.c
@@ -19,9 +19,7 @@ double consecutiveSum(int step, double max) {
 int main() {
 	int sum = 0;
 	for (int i = 0; i < 1000; i ++){
-		if (i%3 == 0 || i%5==0) {
-			sum += i;
-		}
+		if (i%3 == 0 || i%5==0)  sum += i;
 	}
 	printf("%d",sum);
 	return 0;
